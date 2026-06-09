0782ed5403eac736134f16e1dcde71b6956d3bf5
David Castells-Rufas <david.castells@uab.cat>
2026-06-09 18:43:31 +0200

    stack start

--- Test/isa_tests/isa/test_arith_ADIW.asm
+++ Test/isa_tests/isa/test_arith_ADIW.asm
@@ -1,178 +1,213 @@
 ; ============================================================
-; ADIW instruction test suite for ATmega328P
-; ADIW adds a 6-bit immediate (0..63) to a register pair:
-;   Rd+1:Rd  where Rd in {r24, r26, r28, r30}
-; test_case      -> SRAM location tracking current test
-; final_result   -> 1 = OK, -1 = FAIL
+; ADIW instruction test suite (Manual Trampolines)
 ; ============================================================
-; -------------------------
-; SRAM variables
-; -------------------------
+
 .equ test_case    = 0x0100
 .equ final_result = 0x0101
+.equ stack_start = 0x08FF
 .equ SPH = 0x3E
 .equ SPL = 0x3D
-; -------------------------
-; Reset
-; -------------------------
+
 reset:
-    ; init stack
-    ldi r16, 0x03
+    ldi r16, high(stack_start)
     out SPH, r16
-    ldi r16, 0xFF
+    ldi r16, low(stack_start)
     out SPL, r16
-    ; init state
+    
     ldi r16, 0
     sts test_case, r16
     ldi r16, 1
     sts final_result, r16
+    jmp test1
+
 ; ============================================================
-; TEST 1: simple addition, no carry expected
-; r25:r24 = 0x0010 + 1 = 0x0011, C=0
+; TEST 1: simple addition
 ; ============================================================
 test1:
     ldi r24, 0x10
     ldi r25, 0x00
     adiw r24, 1
-    brcs fail                 ; C must be 0
-    cpi r24, 0x11
-    brne fail
-    cpi r25, 0x00
-    brne fail
-    rcall inc_case
+    brcs t1_fail_path       ; Manual trampoline
+    mov r16, r24
+    cpi r16, 0x11
+    brne t1_fail_path
+    mov r16, r25
+    cpi r16, 0x00
+    brne t1_fail_path
+    call inc_case
+    jmp test2
+t1_fail_path:
+    jmp fail
+
 ; ============================================================
-; TEST 2: carry propagation low->high byte, no carry out
-; r25:r24 = 0x00FF + 1 = 0x0100, C=0
+; TEST 2: carry propagation
 ; ============================================================
 test2:
     ldi r24, 0xFF
     ldi r25, 0x00
     adiw r24, 1
-    brcs fail                 ; carry out of 16-bit result? No
-    cpi r24, 0x00
-    brne fail
-    cpi r25, 0x01
-    brne fail
-    rcall inc_case
+    brcs t2_fail_path
+    mov r16, r24
+    cpi r16, 0x00
+    brne t2_fail_path
+    mov r16, r25
+    cpi r16, 0x01
+    brne t2_fail_path
+    call inc_case
+    jmp test3
+t2_fail_path:
+    jmp fail
+
 ; ============================================================
 ; TEST 3: zero identity
-; r25:r24 = 0x1234 + 0 = 0x1234, C=0
 ; ============================================================
 test3:
     ldi r24, 0x34
     ldi r25, 0x12
     adiw r24, 0
-    brcs fail                 ; C must be 0
-    cpi r24, 0x34
-    brne fail
-    cpi r25, 0x12
-    brne fail
-    rcall inc_case
+    brcs t3_fail_path
+    mov r16, r24
+    cpi r16, 0x34
+    brne t3_fail_path
+    mov r16, r25
+    cpi r16, 0x12
+    brne t3_fail_path
+    call inc_case
+    jmp test4
+t3_fail_path:
+    jmp fail
+
 ; ============================================================
-; TEST 4: maximum immediate (63), no carry out
-; r25:r24 = 0x0001 + 63 = 0x0040, C=0
+; TEST 4: maximum immediate
 ; ============================================================
 test4:
     ldi r24, 0x01
     ldi r25, 0x00
     adiw r24, 63
-    brcs fail                 ; C must be 0
-    cpi r24, 0x40
-    brne fail
-    cpi r25, 0x00
-    brne fail
-    rcall inc_case
+    brcs t4_fail_path
+    mov r16, r24
+    cpi r16, 0x40
+    brne t4_fail_path
+    mov r16, r25
+    cpi r16, 0x00
+    brne t4_fail_path
+    call inc_case
+    jmp test5
+t4_fail_path:
+    jmp fail
+
 ; ============================================================
-; TEST 5: 16-bit wrap-around, carry SET
-; r25:r24 = 0xFFFF + 1 = 0x0000, C=1
+; TEST 5: 16-bit wrap-around
 ; ============================================================
 test5:
     ldi r24, 0xFF
     ldi r25, 0xFF
     adiw r24, 1
-    brcc fail                 ; C must be 1
-    cpi r24, 0x00
-    brne fail
-    cpi r25, 0x00
-    brne fail
-    rcall inc_case
+    brcc t5_fail_path
+    mov r16, r24
+    cpi r16, 0x00
+    brne t5_fail_path
+    mov r16, r25
+    cpi r16, 0x00
+    brne t5_fail_path
+    call inc_case
+    jmp test6
+t5_fail_path:
+    jmp fail
+
 ; ============================================================
-; TEST 6: near overflow, carry SET
-; r25:r24 = 0xFFFE + 63 = 0x003D, C=1
+; TEST 6: near overflow
 ; ============================================================
 test6:
     ldi r24, 0xFE
     ldi r25, 0xFF
     adiw r24, 63
-    brcc fail                 ; C must be 1
-    cpi r24, 0x3D
-    brne fail
-    cpi r25, 0x00
-    brne fail
-    rcall inc_case
+    brcc t6_fail_path
+    mov r16, r24
+    cpi r16, 0x3D
+    brne t6_fail_path
+    mov r16, r25
+    cpi r16, 0x00
+    brne t6_fail_path
+    call inc_case
+    jmp test7
+t6_fail_path:
+    jmp fail
+
 ; ============================================================
-; TEST 7: X register pair (r27:r26), no carry
-; r27:r26 = 0x0064 + 10 = 0x006E, C=0
+; TEST 7: X register pair
 ; ============================================================
 test7:
     ldi r26, 0x64
     ldi r27, 0x00
     adiw r26, 10
-    brcs fail                 ; C must be 0
-    cpi r26, 0x6E
-    brne fail
-    cpi r27, 0x00
-    brne fail
-    rcall inc_case
+    brcs t7_fail_path
+    mov r16, r26
+    cpi r16, 0x6E
+    brne t7_fail_path
+    mov r16, r27
+    cpi r16, 0x00
+    brne t7_fail_path
+    call inc_case
+    jmp test8
+t7_fail_path:
+    jmp fail
+
 ; ============================================================
-; TEST 8: Y register pair (r29:r28), carry propagation, no carry out
-; r29:r28 = 0x00F0 + 16 = 0x0100, C=0
+; TEST 8: Y register pair
 ; ============================================================
 test8:
     ldi r28, 0xF0
     ldi r29, 0x00
     adiw r28, 16
-    brcs fail                 ; C must be 0
-    cpi r28, 0x00
-    brne fail
-    cpi r29, 0x01
-    brne fail
-    rcall inc_case
+    brcs t8_fail_path
+    mov r16, r28
+    cpi r16, 0x00
+    brne t8_fail_path
+    mov r16, r29
+    cpi r16, 0x01
+    brne t8_fail_path
+    call inc_case
+    jmp test9
+t8_fail_path:
+    jmp fail
+
 ; ============================================================
-; TEST 9: Z register pair (r31:r30), carry SET
-; r31:r30 = 0xFFFF + 1 = 0x0000, C=1
+; TEST 9: Z register pair
 ; ============================================================
 test9:
     ldi r30, 0xFF
     ldi r31, 0xFF
     adiw r30, 1
-    brcc fail                 ; C must be 1
-    cpi r30, 0x00
-    brne fail
-    cpi r31, 0x00
-    brne fail
-    rcall inc_case
+    brcc t9_fail_path
+    mov r16, r30
+    cpi r16, 0x00
+    brne t9_fail_path
+    mov r16, r31
+    cpi r16, 0x00
+    brne t9_fail_path
+    jmp success
+t9_fail_path:
+    jmp fail
+
 ; ============================================================
-; SUCCESS
+; Logic & Helpers
 ; ============================================================
 success:
     ldi r16, 1
     sts final_result, r16
-end:
-    rjmp end
-; ============================================================
-; FAILURE
-; final_result = -1, stop execution
-; ============================================================
+    jmp end
+
 fail:
     ldi r16, -1
     sts final_result, r16
-    rjmp end
-; ============================================================
-; increment test_case
-; ============================================================
+    jmp end
+
 inc_case:
     lds r16, test_case
     inc r16
     sts test_case, r16
     ret
+
+end:
+    rjmp end
